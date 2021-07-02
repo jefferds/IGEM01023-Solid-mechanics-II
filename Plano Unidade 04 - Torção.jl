540dd4a25d85fe54b7b2c3d233c81afe4d935617
jefferds <jefferds@gmail.com>
2021-07-02 15:34:30 -0300

    Unidade 04

---
+++ Plano Unidade 04 - Torção.jl
@@ -0,0 +1,91 @@
+### A Pluto.jl notebook ###
+# v0.14.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano da Unidade 04 - Torção
+
+*2. Torção.*
+
+### Subitens
+
+**3. TORÇÃO**
+
+- 3.1 Torção de eixos de seção circular
+- 3.2 Ângulo de torção no regime elástico
+- 3.3 Eixos estaticamente indeterminados
+- 3.4 Projeto de eixos de transmissão
+- 3.5 Concentração de tensões em eixos circulares
+
+## Resultados de aprendizagem esperados
+
+■ Apresentar o conceito de torção em elementos estruturais e componentes de máquinas.
+
+■ Definir tensões de cisalhamento e deformações em eixos de seção circular submetidos a torção.
+
+■ Definir ângulo de giro em termos do torque aplicado, da geometria do eixo e do material.
+
+■ Utilizar as deformações torcionais para resolver problemas indeterminados.
+
+■ Projetar eixos para transmissão de potência.
+
+■ Revisar concentração de tensões e como elas são incluídas nos problemas de torção.
+
+■ Descrever a resposta elástica perfeitamente plástica dos eixos de seção circular.
+
+■ Analisar a torção de elementos de seção não circular.
+
+■ Definir o comportamento de eixos vazados de parede fina.
+
+■ Estabelecer relações interpessoais e trabalhar em equipe; comunicar-se oralmente e ter responsabilidade coletiva.
+
+
+# Roteiro da Unidade
+
+#### Momento Pré
+
+🕐 1. A atividade a ser desenvolvida:
+
+*Apresentar os conceitos relacionados ao capitulo 3 do livro do Beer:*
+
+🕒 2. Apresentação do tema pelo professor.
+
+🕑 3. Ler o *Capítulo 3. TORÇÃO,subitens de 3.1 a 3.5* e postar no Classroom resumo de até uma página do que estudou (Resumo pré);
+
+##### Bibliografia
+
+* DeWolf, John T.; Mazurek, David F.; P. Beer, Ferdinand; Johnston JR., E. Russell. Mecânica dos Materiais. AMGH. Edição do Kindle. 
+
+# Roteiro da aula
+
+#### Momento Aula
+
+🕓 4. Seleção dos temas e subtemas com justificativa para que o que será aprendendido e experienciado durante a unidade (06/07/2021).
+
+🕕 5. Cada grupo divide os subtemas entre os estudantes do grupo; elaboram os estudos dos subtemas (modelos, algoritmos, códigos, simulações e análises); como um painel de especialistas, apresentam ao grupo os conceitos e questões chaves e apresentações formais dos subtemas em cada grupo (reunião do grupo) (até 09/07/2021).
+
+🕕 6. Apresentações de alguns subtemas pelos membros responsávies em cada grupo (seleção por sorteio a critério do professsor) (sala) (09/07/2021).
+
+## Roteiro
+
+#### Momento Pós
+
+🕚 7. Preparação das apresentações de cada um dos grupos e envio do relatório da atividade via classroom (atividade do grupo) (13/07/2021).
+
+🕐 8. Apresentações dos grupos (sala).
+
+🕑 9. Avaliações (sala ou extra sala).
+
+🕒 10. Feedback (sala).
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
