b3e3f6c4c1f08ea1bd22dc28028a595872327c38
jefferds <jefferds@gmail.com>
2021-07-20 13:39:11 -0300

    Unidade 05

---
+++ Plano Unidade 05 - Flexão.jl
@@ -0,0 +1,85 @@
+### A Pluto.jl notebook ###
+# v0.14.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano da Unidade 05 - Flexão
+
+### Subitens
+
+**4. Flexão**
+
+- 4.1 Barra simétrica em flexão pura
+- 4.2 Tensões e deformações no regime elástico
+- 4.3 Deformação em um seção transversal
+- 4.4 Barras constituídas de material composto
+- 4.5 Concertração de tensões
+- 4.7 Carregamento axial excêntrico
+- 4.8 Flexão assimétrica
+- 4.9 Caso geral de carregamento axial excêntrico.
+
+
+## Resultados de aprendizagem esperados
+
+■ Definir deformações e tensões normais em vigas submetidas a flexão pura.
+
+■ Descrever o comportamento de vigas compostas de mais de um material.
+
+■ Rever a concentração de tensões e como estas são consideradas no projeto de vigas.
+
+■ Analisar elementos submetidos a cargas axiais excêntricas, envolvendo as tensões axiais e as tensões de flexão.
+
+■ Rever as vigas submetidas a flexão não simétrica, i.e., em que a flexão não ocorre em um plano de simetria.
+
+■ Estabelecer relações interpessoais e trabalhar em equipe; comunicar-se oralmente e ter responsabilidade coletiva.
+
+
+# Roteiro da Unidade
+
+#### Momento Pré
+
+🕐 1. A atividade a ser desenvolvida (20/07/2021):
+
+*Apresentar os conceitos relacionados ao capitulo 4 do livro do Beer:*
+
+🕒 2. Apresentação do tema pelo professor (20/07/2021):.
+
+🕑 3. Ler o *Capítulo 4. FLEXÃO PURA, subitens de 4.1 a 4.9 (*) e postar no Classroom resumo de até uma página do que estudou (Resumo pré) (Até 23/07/2021);
+
+##### Bibliografia
+
+* DeWolf, John T.; Mazurek, David F.; P. Beer, Ferdinand; Johnston JR., E. Russell. Mecânica dos Materiais. AMGH. Edição do Kindle. 
+
+# Roteiro da aula
+
+#### Momento Aula
+
+🕓 4. Seleção dos temas e subtemas com justificativa para que o que será aprendendido e experienciado durante a unidade (Até 23/07/2021).
+
+🕕 5. Cada grupo divide os subtemas entre os estudantes do grupo; elaboram os estudos dos subtemas (modelos, algoritmos, códigos, simulações e análises); como um painel de especialistas, apresentam ao grupo os conceitos e questões chaves e apresentações formais dos subtemas em cada grupo (reunião do grupo) (até 27/07/2021).
+
+🕕 6. Apresentações de alguns subtemas pelos membros responsávies em cada grupo (seleção por sorteio a critério do professsor) (sala) (27/07/2021).
+
+## Roteiro
+
+#### Momento Pós
+
+🕚 7. Preparação das apresentações de cada um dos grupos e envio do relatório da atividade via classroom (atividade do grupo) (Até 23/07/2021).
+
+🕐 8. Apresentações dos grupos (Até 23/07/2021).
+
+🕑 9. Avaliações (sala ou extra sala) (Até 23/07/2021)..
+
+🕒 10. Feedback (sala) (Até 23/07/2021).
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╠═4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
