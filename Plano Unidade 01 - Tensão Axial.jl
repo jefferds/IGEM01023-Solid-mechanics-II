a2d598ce07b2241d2987b50c8014312a6fddd262
jefferds <jefferds@gmail.com>
2022-04-07 08:42:04 -0300

    Atualização dos planos da disciplina e da unidade 01 - 07abr2022

---
+++ Plano Unidade 01 - Tensão Axial.jl
@@ -0,0 +1,118 @@
+### A Pluto.jl notebook ###
+# v0.19.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano da Unidade 01 de Mecânica dos Sólidos II
+
+#### INTRODUÇÃO — O CONCEITO DE TENSÃO
+
+## Ementa da Disciplina:
+
+### Tema relacionado na ementa:
+
+1. Diagramas de Força Axial, Cortante e de Momentos.
+
+2. Tensão e Deformação.
+
+## Resultados de aprendizagem esperados
+
+✔ Compreender os conceitos de tensão axil.
+
+✔ **Apresentar a modelagem, código e simulações para problemas envolvendo tensões axiais**. 
+
+✔ Utilizar as ferramentas $(html"<a href='https://code.visualstudio.com/' target='_blank'>Vscode</a>") e $(html"<a href='https://julialang.org/' target='_blank'>Linguagem Julia</a>") e $(html"<a href='https://github.com/fonsp/Pluto.jl' target='_blank'>Pluto.jl</a>") para o memorial de cálculos e  apresentação de relatórios.
+
+✔ Interpretar e otimizar resultados.
+
+✔ Desenvolver estratégias para solução de problemas.
+
+✔ Estabelecer relações interpessoais.
+
+✔ Trabalhar em equipe.
+
+✔ Comunicar-se oralmente.
+
+✔ Ter responsabilidade coletiva.
+
+
+# Roteiro da Unidade
+
+#### Momento Pré
+
+🕐 1. O professor anuncia por meio de uma orientação por escrito (Sala):
+
+> - Apresentar o tema _**Tensão Axial, tensões em barras e estruturas e tensão em condições gerais de carregamento**_.
+
+> O relatório deve apresentar 3 etapas:
+
+> 1. Introdução com conceitos e definições.
+> 2. Modelagem com memorial de cálculo (usar a linguagem Julia para os cálculos).
+> 3. Simulações análises.
+
+🕑  2. (45 min) Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de até uma página do que estudou (Resumo pré);
+
+##### Bibliografia
+
+* BEER, Ferdinand P. et al. Mecânica dos materiais. Amgh, 2011.
+
+* DeWolf, John T.; Mazurek, David F.; P. Beer, Ferdinand; Johnston JR., E. Russell. Mecânica dos Materiais. AMGH. Edição do Kindle. 
+
+# Roteiro da aula
+
+#### Momento Aula
+
+🕒 3. (30 min) Apresentação do tema pelo professor.
+
+🕓 4. (30 min) Promoção de discussão sobre os interesses no tema que será estudado. O que querem aprender e experienciar durante a unidade? (Sala).
+
+🕔 5. (20 min) Seleção de problemas proposto do livro (reunião do grupo).
+
+🕕 6. (15 min) Cada grupo divide os problemas proposto dentro do grupo (reunião do grupo).
+
+🕖 7. (90 min) Resolver os problemas propostos no livro pelos membros do grupo (estudo individual).
+
+🕗 8. (30 min) Os membros do grupo discutem os problemas propostos e resolvidos (reunião do grupo).
+
+## Roteiro
+
+#### Momento Pós
+
+🕚 9. (30 min) Preparação das apresentações de cada um dos grupos (reunião do grupo).
+
+🕛 10. (10 min) Envio via Classroom do relatório da atividade (atividade do grupo).
+
+🕐 11. (10 min/grupo) Apresentações de cada um dos grupos (sala).
+
+🕑 12. (20 min/grupo) Avaliações (sala ou extra sala).
+
+🕒 13. (10 min/grupo) Feedback (sala).
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╠═4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
